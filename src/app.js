1eb6b1d88b0e8fda982caa885195f186855c3e03
dbvertex <php7dbvertex@gmail.com>
2026-06-18 18:05:06 +0530

    added

--- src/app.js
+++ src/app.js
@@ -43,7 +43,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://pearlines-frontend-admin-tdhr.onrender.com",
+    ],
+    
     credentials: true,
   }),
 );
